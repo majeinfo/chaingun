038473fda32a93d7572fac1faa52c231bdc9579d
JD <sys@maje.biz>
2019-07-23 16:03:16 +0400

    Implement #16 - add a DNS Cache to reduce requests - cache can be disabled

---
+++ player/src/github.com/majeinfo/chaingun/utils/resolver.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"math/rand"
+	"net"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	addrCache = make(map[string][]string, 50)
+)
+
+func GetServerAddress(serverName string) (string, bool) {
+	// Check if address is already in cache, otherwise try to fill the cache
+	if values, err := addrCache[serverName]; err == true {
+		return getAddr(values), true
+	}
+
+	if addrs, err := net.LookupHost(serverName); err == nil {
+		addrCache[serverName] = addrs
+		return getAddr(addrs), true
+	}
+
+	log.Errorf("Could not resolve the Server Name: %s", serverName)
+	return "", false
+}
+
+func getAddr(addrs []string) string {
+	if len(addrs) == 1 {
+		return addrs[0]
+	}
+
+	idx := rand.Intn(len(addrs))
+	return addrs[idx]
+}
