39ec01d7cfbcc5fd6e12c34b1e78038e1ac26b39
JD <sys@maje.biz>
2019-07-24 17:18:54 +0400

    Fix bug in TCP and UDP Action handling

--- player/src/github.com/majeinfo/chaingun/action/tcpreq.go
+++ player/src/github.com/majeinfo/chaingun/action/tcpreq.go
@@ -1,53 +1,54 @@
 package action
 
 import (
-    "fmt"
-    "github.com/majeinfo/chaingun/reporter"
-    log "github.com/sirupsen/logrus"
-    "net"
-    "time"
-)
+	"bufio"
+	"fmt"
+	"net"
+	"time"
 
-var conn net.Conn
+	"github.com/majeinfo/chaingun/reporter"
+	log "github.com/sirupsen/logrus"
+)
 
 // DoTCPRequest accepts a TcpAction and a one-way channel to write the results to.
 func DoTCPRequest(tcpAction TCPAction, resultsChannel chan reporter.SampleReqResult, sessionMap map[string]string, vulog *log.Entry) {
 
-    address := SubstParams(sessionMap, tcpAction.Address, vulog)
-    payload := SubstParams(sessionMap, tcpAction.Payload, vulog)
+	address := SubstParams(sessionMap, tcpAction.Address, vulog)
+	payload := SubstParams(sessionMap, tcpAction.Payload, vulog)
 
-    if conn == nil {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Errorf("TCP socket could not be created, error: %s", err)
+		return
+	}
+	// conn.SetDeadline(time.Now().Add(100 * time.Millisecond))
+	defer conn.Close()
 
-        _, err := net.Dial("tcp", address)
-        if err != nil {
-            log.Errorf("TCP socket closed, error: %s", err)
-            conn = nil
-            return
-        }
-        // conn.SetDeadline(time.Now().Add(100 * time.Millisecond))
-    }
+	start := time.Now()
 
-    start := time.Now()
+	if _, err := fmt.Fprintf(conn, payload); err != nil {
+		log.Errorf("TCP request failed with error: %s", err)
+	} else {
+		// We do not read all the returned bytes
+		buffer := make([]byte, 1000)
+		reader := bufio.NewReader(conn)
+		nbytes, _ := reader.Read(buffer)
+		log.Debugf("%d bytes read !", nbytes)
+	}
 
-    _, err := fmt.Fprintf(conn, payload+"\r\n")
-    if err != nil {
-        log.Errorf("TCP request failed with error: %s", err)
-        conn = nil
-    }
-
-    elapsed := time.Since(start)
-    resultsChannel <- buildTCPResult(sessionMap["UID"], 0, 200, elapsed.Nanoseconds(), tcpAction.Title)
+	elapsed := time.Since(start)
+	resultsChannel <- buildTCPResult(sessionMap["UID"], 0, 200, elapsed.Nanoseconds(), tcpAction.Title)
 }
 
 func buildTCPResult(vid string, contentLength int, status int, elapsed int64, title string) reporter.SampleReqResult {
-    sampleReqResult := reporter.SampleReqResult{
-        Vid:     vid,
-        Type:    "TCP",
-        Latency: elapsed,
-        Size:    contentLength,
-        Status:  status,
-        Title:   title,
-        When:    time.Since(reporter.SimulationStart).Nanoseconds(),
-    }
-    return sampleReqResult
+	sampleReqResult := reporter.SampleReqResult{
+		Vid:     vid,
+		Type:    "TCP",
+		Latency: elapsed,
+		Size:    contentLength,
+		Status:  status,
+		Title:   title,
+		When:    time.Since(reporter.SimulationStart).Nanoseconds(),
+	}
+	return sampleReqResult
 }
--- player/src/github.com/majeinfo/chaingun/action/udpreq.go
+++ player/src/github.com/majeinfo/chaingun/action/udpreq.go
@@ -1,61 +1,53 @@
 package action
 
 import (
-    "fmt"
-    "github.com/majeinfo/chaingun/reporter"
-    log "github.com/sirupsen/logrus"
-    "net"
-    "time"
-)
+	"bufio"
+	"fmt"
+	"net"
+	"time"
 
-var udpconn *net.UDPConn
+	"github.com/majeinfo/chaingun/reporter"
+	log "github.com/sirupsen/logrus"
+)
 
 // DoUDPRequest accepts a UdpAction and a one-way channel to write the results to.
 func DoUDPRequest(udpAction UDPAction, resultsChannel chan reporter.SampleReqResult, sessionMap map[string]string, vulog *log.Entry) {
 
-    address := SubstParams(sessionMap, udpAction.Address, vulog)
-    payload := SubstParams(sessionMap, udpAction.Payload, vulog)
+	address := SubstParams(sessionMap, udpAction.Address, vulog)
+	payload := SubstParams(sessionMap, udpAction.Payload, vulog)
 
-    if udpconn == nil {
-        ServerAddr, err := net.ResolveUDPAddr("udp", address) //"127.0.0.1:10001")
-        if err != nil {
-            log.Errorf("Error ResolveUDPAddr remote: %s", err.Error())
-        }
+	conn, err := net.Dial("udp", address)
+	if err != nil {
+		log.Errorf("UDP socket could not be created, error: %s", err.Error())
+		return
+	}
+	// conn.SetDeadline(time.Now().Add(100 * time.Millisecond))
 
-        LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
-        if err != nil {
-            log.Errorf("Error ResolveUDPAddr local: %s", err.Error())
-        }
+	defer conn.Close()
+	start := time.Now()
 
-        udpconn, err = net.DialUDP("udp", LocalAddr, ServerAddr)
-        if err != nil {
-            log.Errorf("Error Dial: %s", err.Error())
-        }
-    }
-    //defer Conn.Close()
-    start := time.Now()
-    if udpconn != nil {
-        _, err := fmt.Fprintf(udpconn, payload+"\r\n")
-        if err != nil {
-            log.Errorf("UDP request failed with error: %s", err)
-            udpconn = nil
-        }
-    }
+	if _, err := fmt.Fprintf(conn, payload); err != nil {
+		log.Errorf("UDP request failed with error: %s", err)
+	} else {
+		// We do not read all the returned bytes
+		buffer := make([]byte, 1000)
+		reader := bufio.NewReader(conn)
+		reader.Read(buffer)
+	}
 
-    elapsed := time.Since(start)
-    resultsChannel <- buildUDPResult(sessionMap["UID"], 0, 200, elapsed.Nanoseconds(), udpAction.Title)
-
+	elapsed := time.Since(start)
+	resultsChannel <- buildUDPResult(sessionMap["UID"], 0, 200, elapsed.Nanoseconds(), udpAction.Title)
 }
 
 func buildUDPResult(vid string, contentLength int, status int, elapsed int64, title string) reporter.SampleReqResult {
-    sampleReqResult := reporter.SampleReqResult{
-        Vid:     vid,
-        Type:    "UDP",
-        Latency: elapsed,
-        Size:    contentLength,
-        Status:  status,
-        Title:   title,
-        When:    time.Since(reporter.SimulationStart).Nanoseconds(),
-    }
-    return sampleReqResult
+	sampleReqResult := reporter.SampleReqResult{
+		Vid:     vid,
+		Type:    "UDP",
+		Latency: elapsed,
+		Size:    contentLength,
+		Status:  status,
+		Title:   title,
+		When:    time.Since(reporter.SimulationStart).Nanoseconds(),
+	}
+	return sampleReqResult
 }
