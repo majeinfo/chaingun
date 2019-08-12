d0ba7061654f07de96d52b3adb4ca5cfc039c636
JD <jd@maje.biz>
2019-08-12 10:06:29 +0200

    Remove a race condition on IP address cache

--- player/src/github.com/majeinfo/chaingun/utils/resolver.go
+++ player/src/github.com/majeinfo/chaingun/utils/resolver.go
@@ -3,16 +3,21 @@ package utils
 import (
 	"math/rand"
 	"net"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
 )
 
 var (
 	addrCache = make(map[string][]string, 50)
+	cacheLock sync.Mutex
 )
 
 func GetServerAddress(serverName string) (string, bool) {
 	// Check if address is already in cache, otherwise try to fill the cache
+	cacheLock.Lock()
+	defer cacheLock.Unlock()
+
 	if values, err := addrCache[serverName]; err == true {
 		return getAddr(values), true
 	}
