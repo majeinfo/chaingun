f04567123d2316fccc1bfac6d100266fd7664dce
JD <jd@maje.biz>
2020-01-02 18:55:26 +0100

    Fix a bug in getAddr(): the port number was lost when converting hostname to IP address

--- player/src/github.com/majeinfo/chaingun/utils/resolver.go
+++ player/src/github.com/majeinfo/chaingun/utils/resolver.go
@@ -14,19 +14,22 @@ var (
 	cacheLock sync.Mutex
 )
 
+// GetServerAddress fills and manages a cache on host/IP address
+// to minimize DNS requests
 func GetServerAddress(serverName string) (string, bool) {
-	// Discard the port if given
-	serverName = strings.Split(serverName, ":")[0]
+	// Split the serverName into two parts: the Host and the Port (may be empty)
+	parts := strings.Split(serverName, ":")
+	host := parts[0]
 
 	// Check if address is already in cache, otherwise try to fill the cache
 	cacheLock.Lock()
 	defer cacheLock.Unlock()
 
-	if values, err := addrCache[serverName]; err == true {
-		return getAddr(values), true
+	if values, err := addrCache[host]; err == true {
+		return appendPort(getAddr(values), parts), true
 	}
 
-	if addrs, err := net.LookupHost(serverName); err == nil {
+	if addrs, err := net.LookupHost(host); err == nil {
 		// Remove IPv6 addresses
 		addrs4 := make([]string, 0)
 		for _, addr := range addrs {
@@ -34,11 +37,11 @@ func GetServerAddress(serverName string) (string, bool) {
 				addrs4 = append(addrs4, addr)
 			}
 		}
-		addrCache[serverName] = addrs4
-		return getAddr(addrs4), true
+		addrCache[host] = addrs4
+		return appendPort(getAddr(addrs4), parts), true
 	}
 
-	log.Errorf("Could not resolve the Server Name: %s", serverName)
+	log.Errorf("Could not resolve the Server Name: %s", host)
 	return "", false
 }
 
@@ -49,6 +52,14 @@ func getAddr(addrs []string) string {
 	}
 
 	idx := rand.Intn(len(addrs))
-	log.Debugf("getAddr returns:", addrs[idx])
+	log.Debugf("getAddr returns: %s", addrs[idx])
 	return addrs[idx]
+}
+
+func appendPort(host string, parts []string) string {
+	// Concatenate the port with the host name
+	if len(parts) > 1 {
+		return host + ":" + parts[1]
+	}
+	return host
 }
