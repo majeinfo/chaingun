2c55bc849874d911a6426d9f40b58b5e1f875f35
JD <jd@maje.biz>
2020-01-02 15:50:02 +0100

    Changed /bin/bash into /bin/sh because bash not installed in docker-dind

--- tests/run_web_server.sh
+++ tests/run_web_server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 docker container run -d -p 8000:80 -v `pwd`/server:/var/www/html php:5.6-apache
 
--- tests/test_standalone_player.sh
+++ tests/test_standalone_player.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 PLAYER=../player/bin/player
 #VERBOSE=--verbose
