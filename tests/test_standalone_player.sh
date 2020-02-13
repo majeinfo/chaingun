b02d7da0cd0922d7c8698afb5dd35672bb19506d
JD <jd@maje.biz>
2020-02-13 08:52:52 +0100

    remove unuseful lines

--- tests/test_standalone_player.sh
+++ tests/test_standalone_player.sh
@@ -4,11 +4,6 @@ PLAYER=../player/bin/player
 #VERBOSE=--verbose
 ERRORS=0
 
-pwd
-ls -laR /builds
-test -f /builds/majeinfo/chaingun/player/bin/player
-PLAYER=/builds/majeinfo/chaingun/player/bin/player
-
 Arg_Error() {
 	if $PLAYER $1 2>&1 | tee $$.out | grep "$2" >/dev/null 2>&1; then
 		echo '[OK]' $1
