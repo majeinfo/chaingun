01bb05e8fec87a3b7206acd6f30e66726f98f8cb
JD <jd@maje.biz>
2023-08-24 13:07:52 +0200

    add substparams versions

--- player/src/action/variableprocessor.go
+++ player/src/action/variableprocessor.go
@@ -12,6 +12,14 @@ var re = regexp.MustCompile("\\$\\{([a-zA-Z0-9_]{0,})\\}")
 
 // SubstParams compute the result of variables interpolation
 func SubstParams(sessionMap map[string]string, textData string, vulog *log.Entry) string {
+	return SubstParams2(sessionMap, textData, vulog, false)
+}
+
+func SubstParamsNoEscape(sessionMap map[string]string, textData string, vulog *log.Entry) string {
+	return SubstParams2(sessionMap, textData, vulog, true)
+}
+
+func SubstParams2(sessionMap map[string]string, textData string, vulog *log.Entry, no_escape bool) string {
 	if strings.ContainsAny(textData, "${") {
 		res := re.FindAllStringSubmatch(textData, -1)
 		for _, v := range res {
@@ -19,7 +27,11 @@ func SubstParams(sessionMap map[string]string, textData string, vulog *log.Entry
 			if _, err := sessionMap[v[1]]; !err {
 				vulog.Errorf("Variable ${%s} not set", v[1])
 			}
-			textData = strings.Replace(textData, "${"+v[1]+"}", url.QueryEscape(sessionMap[v[1]]), 1)
+			if no_escape {
+				textData = strings.Replace(textData, "${"+v[1]+"}", sessionMap[v[1]], 1)
+			} else {
+				textData = strings.Replace(textData, "${"+v[1]+"}", url.QueryEscape(sessionMap[v[1]]), 1)
+			}
 		}
 		return textData
 	}
